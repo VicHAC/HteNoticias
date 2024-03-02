e008a7835d67b32e7bc77b56ae3d268dc480c748
Victor A <victorac.palomita@gmail.com>
2024-03-02 08:41:34 -0600

    Social Media Added

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -7,15 +6,15 @@ const Footer = () => {
       <div className="container mx-auto text-center">
         <p>© {new Date().getFullYear()} HorizonteNoticias. Todos los derechos reservados.</p>
         <div className="mt-2">
-          <Link to="/" className=" hover:text-black mx-2">
+          <a href="https://twitter.com/HteNoticias" target="_blank" rel="noopener noreferrer" className=" hover:text-black mx-2">
             X Twitter
-          </Link>
-          <Link to="/" className="text-blue-400 hover:text-blue-600 mx-2">
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=61556680932353" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-600 mx-2">
             Facebook
-          </Link>
-          <Link to="/" className="text-pink-400 hover:text-pink-600 mx-2">
+          </a>
+          <a href="https://www.instagram.com/hte_noticias/" target="_blank" rel="noopener noreferrer" className="text-pink-400 hover:text-pink-600 mx-2">
             Instagram
-          </Link>
+          </a>
         </div>
       </div>
     </footer>
